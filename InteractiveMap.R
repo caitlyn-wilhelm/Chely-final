88df811000c6549077946af08e3f745d17e1a86d
Ethan Tarr <ebt0640@yahoo.com>
2016-12-03 23:57:42 -0800

    Finished Interactive Map
    close #3 Interactive Map finished

---
+++ InteractiveMap.R
@@ -0,0 +1,60 @@
+library(dplyr)
+library(leaflet)
+library(rgdal)
+
+#Read in data from csv in Data folder
+data <- read.csv("Data/Student_Weight_Status_Category_Reporting_Results__Beginning_2010.csv")
+
+#modify data to fit for a map
+#-----------------------------
+#filter down to county wide district total values
+data <- filter(data, grepl("[^N/A]", COUNTY), AREA.TYPE == "COUNTY", GRADE.LEVEL == "DISTRICT TOTAL")
+
+#remove percentage signs from values
+data$PCT.OVERWEIGHT <- as.numeric(sub("%", "", data$PCT.OVERWEIGHT))
+data$PCT.OBESE <- as.numeric(sub("%", "", data$PCT.OBESE))
+data$PCT.OVERWEIGHT.OR.OBESE <- as.numeric(sub("%", "", data$PCT.OVERWEIGHT.OR.OBESE))
+
+#group and summarize data into a dataset with only useful information
+data <- group_by(data, LOCATION.CODE) %>% 
+  summarize(County = COUNTY[1], Region = REGION[1], No.Overweight = sum(NO..OVERWEIGHT),
+            Pct.Overweight = mean(PCT.OVERWEIGHT), No.Obese = sum(NO..OBESE),
+            Pct.Obese = mean(PCT.OBESE), No.Overweight.Or.Obese = sum(NO..OVERWEIGHT.OR.OBESE),
+            Pct.Overweight.Or.Obese = mean(PCT.OVERWEIGHT.OR.OBESE))
+
+#changed name of one county to fit with map data
+data[40,2] <- "ST. LAWRENCE"
+#----------------------------
+
+# read in map data as a Spatial Polygons Data Frame
+map.data <- readShapeSpatial("Data/NY_Map/NY_counties_clip.shp")
+
+#change county names to uppercase for merge
+map.data$NAME <- toupper(map.data$NAME)
+
+#merge map data with county data
+combined.map.data <- merge(map.data, data, by.x = "NAME", by.y = "County")
+
+#create map popups
+county_popup <- paste0(combined.map.data$NAME, " county", 
+                      "<br>with an obesity rate of ", 
+                      combined.map.data$Pct.Obese)
+
+#create color palette
+pal <- colorNumeric(
+  palette = "Blues",
+  domain = combined.map.data$Pct.Obese
+)
+
+#create map with legend
+map <- leaflet(combined.map.data) %>%
+  addPolygons(
+              fillOpacity = 0.8, 
+              color = "#BDBDC3", 
+              weight = 1, fillColor = ~pal(Pct.Obese), popup = county_popup
+  ) %>% 
+  addLegend("bottomright", pal = pal, values = ~Pct.Obese,
+              title = "Student Percentage Obese",
+              labFormat = labelFormat(suffix = "%"),
+              opacity = 1
+  )
